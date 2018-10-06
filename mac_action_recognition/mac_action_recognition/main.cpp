da9b0f656cd6965053f912141c7ca1b872d29d9c
Ddd <752042257@qq.com>
2018-10-06 13:01:10 +0800

    initialization

---
+++ mac_action_recognition/mac_action_recognition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mac_action_recognition
+//
+//  Created by Ddd on 2018/10/6.
+//  Copyright © 2018年 Ddd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
